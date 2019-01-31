780021cfc025b040aba5fe5b1e6f4abeb915adce
Dmitriy.Lipgart <dmitriylipgart@gmail.com>
2019-01-31 18:14:40 +0300

    branch with simple console app

---
+++ src/main/java/com/itechart/crp/configuration/AppConfig.java
@@ -0,0 +1,13 @@
+package com.itechart.crp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.jmx.support.RegistrationPolicy;
+
+@Configuration
+@EnableMBeanExport(registration= RegistrationPolicy.IGNORE_EXISTING)
+public class AppConfig {
+
+
+
+}
